8636365043f092d5db2c3ab03b78495bc4fa384e
sendsay <sendsay@yahoo.com>
2019-06-26 14:25:59 +0300

    Added scroll for all anchors

--- resources/js/script.js
+++ resources/js/script.js
@@ -4,18 +4,30 @@ $(function() {
     $(window).scroll(function() {   
       if($(this).scrollTop() > 300) {   
         $('.icon-bar').animate({left: "0px"});  //hide side panel
-        $('#toTop').fadeIn();  
+        $('#toTop').fadeIn(300);  
       } else {   
         if($(this).scrollTop() < 10) {
         $('.icon-bar').animate({left: "-60px"});  //hide side panel
-        $('#toTop').fadeOut();   
+        $('#toTop').fadeOut(300);   
         }
       }         
     });
      
     $('#toTop').click(function() {   
-      $('body,html, footer').animate({scrollTop:0},600);
+      $('body, html, footer').animate({scrollTop:0},1000);
       $('.icon-bar').animate({left: "-60px"});  //hide side panel   
       
     });   
+});
+
+// scroll for all anchors
+$(document).ready(function(){
+    $('a[href*=#]').bind("click", function(e){
+    var anchor = $(this);
+    $('html, body').stop().animate({
+    scrollTop: $(anchor.attr('href')).offset().top
+    }, 1000);
+    e.preventDefault();
+    });
+    return false;
 });
