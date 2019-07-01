54937f97fd18ab408ae1f7d00d57f209a558fb5c
sendsay <sendsay@yahoo.com>
2019-07-01 08:54:25 +0300

    Added scroll time var

--- resources/js/script_aboutme.js
+++ resources/js/script_aboutme.js
@@ -1,22 +1,23 @@
 var flagDown = true;  //show side panel flag
+var scrollTime = 1000;  // scrolling page time
+
 var offset = 0; // offset page
 
-    $(window).scroll(function() {
-        offset = $(window).scrollTop();
+$(window).scroll(function() {
+    offset = $(window).scrollTop();
 
-        if(offset <= 300) {         
-          offset     = offset / 3.33;
-          $('#toTop').css({
-              'transform': 'rotate(' + offset + 'deg)'
-          });
-        }
-    });
-
+    if(offset <= 300) {         
+      offset     = offset / 3.33;
+      $('#toTop').css({
+          'transform': 'rotate(' + offset + 'deg)'
+      });
+    }
+});
      
-    $('#toTop').click(function() {   
-      if (offset > 0) {
-        $('body, html, footer').animate({scrollTop:0},1000);   
-      } else {
-        history.back();
-      } 
-    });   
+$('#toTop').click(function() {   
+  if (offset > 0) {
+    $('body, html, footer').animate({scrollTop:0}, scrollTime);   
+  } else {
+    history.back();
+  } 
+});   
