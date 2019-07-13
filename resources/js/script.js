2b03967c0afe70d93fc116c2bfe10841a1fe7b8f
sendsay <sendsay@yahoo.com>
2019-07-13 11:54:48 +0300

    dosnt work js on about opage

--- resources/js/script.js
+++ resources/js/script.js
@@ -5,51 +5,44 @@ var pageName = location.href;  //get page name
 var offset = 0; // offset page
 
 //always get page name
-pageName = pageName.substr(pageName.lastIndexOf("/")); 
+pageName = pageName.substr(pageName.lastIndexOf("/") + 1); 
 //set year to footer
 document.getElementById("year").innerHTML = year.getFullYear();
 
 //show buttons toTop and iconBar
 $(window).scroll(function() { 
-  switch (pageName) {
-    case "/index.html" : {
-      if($(this).scrollTop() > 300) { 
-        if (flagDown == false) {
-          $('.icon-bar').animate({ left: "0px" });
-          $('#toTop').animate({ bottom: "30px"});  
-          flagDown = true;
-        }    
-      } else {   
-        if (flagDown == true) {
-          $('.icon-bar').animate({ left: "-100px" });
-          $('#toTop').animate({ bottom: "-80px"});          
-          flagDown = false;
-        }
-      } 
-    } 
-    break;
 
-    //rotate button toTop on page
-    case "/aboutme.html" : {
-      offset = $(window).scrollTop();
-      if(offset <= 300) {         
-        offset     = offset / 3.33;
-        $('#toTopImg').css({
-            'transform': 'rotate(' + offset + 'deg)'
-        });
-      }
-    } 
-    break;
-  
-    default:
-      break;
-  }
+  if($(this).scrollTop() > 300) { 
+    if (flagDown == false) {          
+      if (pageName == "index.html") {$('.icon-bar').animate({ left: "0px" });}
+      $('#toTop').animate({ bottom: "30px"});
+      flagDown = true;
+    }    
+  } else {   
+    if (flagDown == true) {
+      if (pageName == "index.html") {$('.icon-bar').animate({ left: "-100px" });}
+      $('#toTop').animate({ bottom: "-80px"});        
+      flagDown = false;
+    }
+  }     
+
+  //rotate button toTop on page
+  if (pageName == "aboutme.html") {
+    
+    offset = $(window).scrollTop();
+    if(offset <= 300) {         
+      offset     = offset / 3.33;
+      $('#toTopImg').css({
+          'transform': 'rotate(' + offset + 'deg)'
+      });
+    }
+  }   
 });
      
 //click toTOP
 $('#toTop').click(function() {
   switch (pageName) {
-    case "/index.html":
+    case "index.html":
       $('body, html, footer').animate({scrollTop:0}, scrollTime);
       break;
   
@@ -60,9 +53,7 @@ $('#toTop').click(function() {
         history.back();
       } 
       break;
-  } 
-
-        
+  }         
 });   
 
 // click SHOP ICONS (index.html)
@@ -70,15 +61,14 @@ $('.section-shop').click(function() {
   document.location.href = "shop_select.html";
 });
 
-
 // scroll for all anchors
 $(document).ready(function(){
-    $('a[href*=\\#]').bind("click", function(e){
+  $('a[href*=\\#]').bind("click", function(e){
     var anchor = $(this);
     $('html, body').stop().animate({
     scrollTop: $(anchor.attr('href')).offset().top
     }, scrollTime);
     e.preventDefault();
-    });
-    return false;
+  });
+  return false;
 });
